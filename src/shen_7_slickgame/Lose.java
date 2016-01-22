2c3e92217cea19013af6eefee5f5bfa40405fcf2
Xiangyu Shen <xshen2013@gmail.com>
2016-01-22 16:14:54 -0600

    Finished Game.

---
+++ src/shen_7_slickgame/Lose.java
@@ -0,0 +1,202 @@
+package shen_7_slickgame;
+
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Image;
+
+
+import org.newdawn.slick.Game;
+
+
+import org.newdawn.slick.GameContainer;
+
+
+import org.newdawn.slick.Graphics;
+
+
+import org.newdawn.slick.Input;
+
+
+import org.newdawn.slick.SlickException;
+
+
+import org.newdawn.slick.state.BasicGameState;
+
+
+import org.newdawn.slick.state.StateBasedGame;
+
+
+import org.newdawn.slick.state.transition.FadeInTransition;
+
+
+import org.newdawn.slick.state.transition.FadeOutTransition;
+
+
+
+public class Lose extends BasicGameState {
+
+
+    
+
+
+    private StateBasedGame game;
+    public Image startimage;
+
+    
+
+
+     public Lose(int xSize, int ySize) {
+
+
+
+    }
+
+
+
+    
+
+
+    public void init(GameContainer container, StateBasedGame game)
+
+
+            throws SlickException {
+    	startimage = new Image("res/MyDungeon/Lose.png");
+
+
+        this.game = game;
+
+
+
+// TODO AutoÃ¢â‚¬Âgenerated method stub
+
+
+    }
+
+
+
+   
+
+
+    public void render(GameContainer container, StateBasedGame game, Graphics g)
+
+
+            throws SlickException {
+    	
+    	startimage.draw();
+
+
+
+// TODO AutoÃ¢â‚¬Âgenerated method stub
+
+
+        g.setColor(Color.white);
+
+
+        //g.drawString("You LOSE!", 450, 200);
+//        g.drawString("press 1 to try again", 400, 320);
+
+
+       
+
+
+    }
+
+
+
+    
+
+
+    public void update(GameContainer container, StateBasedGame game, int delta)
+
+
+            throws SlickException {
+
+
+// TODO AutoÃ¢â‚¬Âgenerated method stub
+
+
+    }
+
+
+
+  
+
+
+    public int getID() {
+
+
+// TODO AutoÃ¢â‚¬Âgenerated method stub
+
+
+        return 2;
+
+
+    }
+
+
+
+    @Override
+
+
+    public void keyReleased(int key, char c) {
+
+
+        switch (key) {
+
+
+            case Input.KEY_1:
+
+                Player.time  = 350000;
+                Player.speed = .4f;
+                Player.health = 100;
+                BarbarianHorde.counter = 0;
+                Itemwin.isvisible = true;
+                Player.x = 49f;
+                Player.y = 86f;
+                BarbarianHorde.SandBoss.isVisible = true;
+                BarbarianHorde.Boss.isVisible = true;
+                BarbarianHorde.Boss2.isVisible = true;
+                BarbarianHorde.MetalBoss.isVisible = true;
+                BarbarianHorde.key1.isvisible = true;
+                BarbarianHorde.gate1.isvisible = true;
+                BarbarianHorde.gate2.isvisible = true;
+                BarbarianHorde.hasKey = false;
+                //redo potions and reset cordinates of player
+                game.enterState(1, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
+
+
+                break;
+
+
+            case Input.KEY_2:
+
+
+// TODO: Implement later
+
+
+                break;
+
+
+            case Input.KEY_3:
+
+
+// TODO: Implement later
+
+
+                break;
+
+
+            default:
+
+
+                break;
+
+
+        }
+
+
+    }
+
+
+}
+
---
+++ src/shen_7_slickgame/Player.java
@@ -0,0 +1,90 @@
+package shen_7_slickgame;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ShadowX
+ */
+public class Player {
+
+	public static float x = 49f;
+
+	public static float y = 86f;
+
+	public static int time = 350000;
+	
+	public static float speed = .4f;
+        
+        public static int health = 100;
+
+	static float hitboxX = x + 4f;
+
+	static float hitboxY = y + 4f;
+
+	private static int startX, startY, width = 30, height = 42;
+
+	public static Shape rect = new Rectangle(getplayershitboxX(),
+			getplayershitboxY(), width, height);
+
+	public static float pdelta;
+
+	public static Animation playeranime;
+
+	public static void setpdelta(float somenum) {
+
+		pdelta = somenum;
+
+	}
+
+	public static float getpdelta() {
+
+		return pdelta;
+
+	}
+
+	public static float getplayersX() {
+
+		return x;
+
+	}
+
+	public static float getplayersY() {
+
+		return y;
+
+	}
+
+	public static float getplayershitboxX() {
+
+		return x + 18f;
+
+	}
+
+	public static float getplayershitboxY() {
+
+		return y + 18f;
+
+	}
+
+	public static void setplayershitboxX() {
+
+		hitboxX = getplayershitboxX();
+
+	}
+
+	public static void setplayershitboxY() {
+
+		hitboxY = getplayershitboxY();
+
+	}
+
+}
