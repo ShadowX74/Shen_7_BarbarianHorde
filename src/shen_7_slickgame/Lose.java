0f0803cfea951d85c9c1dbf01d246a1803c6401b
Xiangyu Shen <xshen2013@gmail.com>
2016-02-02 16:15:00 -0600

    Cleaning up code to make more legible.

--- src/shen_7_slickgame/Lose.java
+++ src/shen_7_slickgame/Lose.java
@@ -1,104 +1,35 @@
 package shen_7_slickgame;
 
-
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Image;
-
-
 import org.newdawn.slick.Game;
-
-
 import org.newdawn.slick.GameContainer;
-
-
 import org.newdawn.slick.Graphics;
-
-
 import org.newdawn.slick.Input;
-
-
 import org.newdawn.slick.SlickException;
-
-
 import org.newdawn.slick.state.BasicGameState;
-
-
 import org.newdawn.slick.state.StateBasedGame;
-
-
 import org.newdawn.slick.state.transition.FadeInTransition;
-
-
 import org.newdawn.slick.state.transition.FadeOutTransition;
 
-
-
 public class Lose extends BasicGameState {
-
-
-    
-
-
     private StateBasedGame game;
     public Image startimage;
-
-    
-
-
-     public Lose(int xSize, int ySize) {
-
-
-
+    public Lose(int xSize, int ySize) {
     }
 
-
-
-    
-
-
-    public void init(GameContainer container, StateBasedGame game)
-
-
-            throws SlickException {
+    public void init(GameContainer container, StateBasedGame game) throws SlickException {
     	startimage = new Image("res/MyDungeon/Lose.png");
-
-
         this.game = game;
-
-
-
-// TODO AutoÃ¢â‚¬Âgenerated method stub
-
-
+        // TODO AutoÃ¢â‚¬Âgenerated method stub
     }
 
-
-
-   
-
-
-    public void render(GameContainer container, StateBasedGame game, Graphics g)
-
-
-            throws SlickException {
-    	
+    public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
     	startimage.draw();
-
-
-
-// TODO AutoÃ¢â‚¬Âgenerated method stub
-
-
+        // TODO AutoÃ¢â‚¬Âgenerated method stub
         g.setColor(Color.white);
-
-
         //g.drawString("You LOSE!", 450, 200);
-//        g.drawString("press 1 to try again", 400, 320);
-
-
-       
-
-
+        //g.drawString("press 1 to try again", 400, 320);
     }
 
 
@@ -146,13 +77,13 @@ public class Lose extends BasicGameState {
 
             case Input.KEY_1:
 
-                Player.time  = 350000;
-                Player.speed = .4f;
-                Player.health = 100;
+                BarbarianHorde.playerguy.time  = 300000;
+                BarbarianHorde.playerguy.speed = .4f;
+                BarbarianHorde.playerguy.health = 100;
                 BarbarianHorde.counter = 0;
                 Itemwin.isvisible = true;
-                Player.x = 49f;
-                Player.y = 86f;
+                BarbarianHorde.playerguy.x = 49f;
+                BarbarianHorde.playerguy.y = 86f;
                 BarbarianHorde.SandBoss.isVisible = true;
                 BarbarianHorde.Boss.isVisible = true;
                 BarbarianHorde.Boss2.isVisible = true;
@@ -160,7 +91,7 @@ public class Lose extends BasicGameState {
                 BarbarianHorde.key1.isvisible = true;
                 BarbarianHorde.gate1.isvisible = true;
                 BarbarianHorde.gate2.isvisible = true;
-                BarbarianHorde.hasKey = false;
+                BarbarianHorde.playerguy.hasKey = false;
                 //redo potions and reset cordinates of player
                 game.enterState(1, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
 
